229f51d266b4753bb679cceb8979655e19784270
Kaustubh Patil <68739277+khpatil19@users.noreply.github.com>
2024-10-24 10:44:25 -0500

    Commit_1

--- form.py
+++ form.py
@@ -1,2 +1,7 @@
-import pandas as pd
-print("Testing app")
+print("Testing app khpatil")
+
+# import pandas as pd
+# from joblib import load
+
+# # Load the model from the file
+# rf_pipeline = load('C:/Users/khpat/Desktop/Desktop Files/LabelMaster/random_forest_regressor.joblib')
